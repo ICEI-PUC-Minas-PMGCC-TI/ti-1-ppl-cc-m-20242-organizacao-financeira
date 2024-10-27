87c9d3395df0946b05f536dd9c57ff8d167e2f78
pedro-maia1337 <pedro.maia1337@gmail.com>
2024-10-27 14:43:25 -0300

    Arrumando a rota de tutorais

--- codigo/public/routes/tutorial.js
+++ codigo/public/routes/tutorial.js
@@ -2,52 +2,61 @@ const express = require("express")
 const router = express.Router()
 const fs = require('fs');
 
-router.get('/videoList.json', (req, res) => {
+app.get('/../db/db.json', (req, res) => {
     fs.readFile('../db/db.json', (err, data) => {
         if (err) {
-            return res.status(500).send('Erro ao ler o arquivo de vídeo.');
+            return res.status(500).send('Erro ao ler o arquivo de tutoriais.');
         }
         res.json(JSON.parse(data));
     });
 });
 
-// Adicionar um novo vídeo
-router.post('/addVideo', (req, res) => {
+// Adicionar um novo tutorial
+app.post('/addVideo', (req, res) => {
     const newVideo = req.body;
 
     fs.readFile('../db/db.json', (err, data) => {
-        let videoList = [];
+        let videoData = { tutoriais: [] };
         if (!err) {
-            videoList = JSON.parse(data);
+            videoData = JSON.parse(data);
         }
 
-        videoList.push(newVideo);
-        fs.writeFile('../db/db.json', JSON.stringify(videoList, null, 2), (err) => {
+        // Definir um novo ID para o tutorial com base no último ID do array
+        const newId = videoData.tutoriais.length > 0 
+            ? videoData.tutoriais[videoData.tutoriais.length - 1].id + 1 
+            : 1;
+
+        // Adicionar o ID ao novo vídeo
+        const tutorial = { id: newId, ...newVideo };
+        videoData.tutoriais.push(tutorial);
+
+        // Salvar o arquivo JSON com o novo tutorial
+        fs.writeFile('../db/db.json', JSON.stringify(videoData, null, 2), (err) => {
             if (err) {
-                return res.status(500).send('Erro ao salvar os dados.');
+                return res.status(500).send('Erro ao salvar o tutorial.');
             }
-            res.status(200).send('Dados salvos com sucesso.');
+            res.status(200).send('Tutorial salvo com sucesso.');
         });
     });
 });
 
-// Deletar um vídeo
+// Deletar um tutorial
 app.delete('/deleteVideo', (req, res) => {
     const index = req.body.index;
 
     fs.readFile('../db/db.json', (err, data) => {
         if (err) {
-            return res.status(500).send('Erro ao ler o arquivo de vídeo.');
+            return res.status(500).send('Erro ao ler o arquivo de tutoriais.');
         }
 
-        let videoList = JSON.parse(data);
-        if (index >= 0 && index < videoList.length) {
-            videoList.splice(index, 1);
-            fs.writeFile('../db/db.json', JSON.stringify(videoList, null, 2), (err) => {
+        let videoData = JSON.parse(data);
+        if (index >= 0 && index < videoData.tutoriais.length) {
+            videoData.tutoriais.splice(index, 1);
+            fs.writeFile('../db/db.json', JSON.stringify(videoData, null, 2), (err) => {
                 if (err) {
-                    return res.status(500).send('Erro ao deletar o vídeo.');
+                    return res.status(500).send('Erro ao deletar o tutorial.');
                 }
-                res.send('Vídeo deletado com sucesso.');
+                res.send('Tutorial deletado com sucesso.');
             });
         } else {
             res.status(400).send('Índice inválido.');
@@ -55,28 +64,29 @@ app.delete('/deleteVideo', (req, res) => {
     });
 });
 
-// Atualizar um vídeo
-router.put('/updateVideo', (req, res) => {
+// Atualizar um tutorial
+app.put('/updateVideo', (req, res) => {
     const { index, video } = req.body;
 
     fs.readFile('../db/db.json', (err, data) => {
         if (err) {
-            return res.status(500).send('Erro ao ler o arquivo de vídeo.');
+            return res.status(500).send('Erro ao ler o arquivo de tutoriais.');
         }
 
-        let videoList = JSON.parse(data);
-        if (index >= 0 && index < videoList.length) {
-            videoList[index] = video;
-            fs.writeFile('../db/db.json', JSON.stringify(videoList, null, 2), (err) => {
+        let videoData = JSON.parse(data);
+        if (index >= 0 && index < videoData.tutoriais.length) {
+            videoData.tutoriais[index] = { ...videoData.tutoriais[index], ...video };
+            fs.writeFile('../db/db.json', JSON.stringify(videoData, null, 2), (err) => {
                 if (err) {
-                    return res.status(500).send('Erro ao atualizar o vídeo.');
+                    return res.status(500).send('Erro ao atualizar o tutorial.');
                 }
-                res.send('Vídeo atualizado com sucesso.');
+                res.send('Tutorial atualizado com sucesso.');
             });
         } else {
             res.status(400).send('Índice inválido.');
         }
     });
 });
+
 
 module.exports = router
