bc0ecbfafafa97cd61ac07b0dfd100fa863afa17
Felipe Quites Lopes <quites@Felipes-MacBook-Air.local>
2024-10-30 21:28:03 -0300

    JavaScript Corrigido para gerar a imagem na preview-box

--- codigo/Cadastro_de_carteiras/public/script.js
+++ codigo/Cadastro_de_carteiras/public/script.js
@@ -1,46 +1,24 @@
-const colorPreview = document.getElementById('preview-box');
-const iconPreview = document.getElementById('icon-preview');
-
-document.querySelectorAll('.color-picker input').forEach(input => {
-    input.addEventListener('change', (event) => {
-        const color = event.target.value;
-        colorPreview.style.backgroundColor = color;
-    });
-});
-
-document.querySelectorAll('.icon-picker input').forEach(input => {
-    input.addEventListener('change', (event) => {
-        const icon = event.target.value;
-        iconPreview.textContent = icon;
-    });
-});
-
-
 function validateForm() {
     const fields = ["nomeConta", "saldo", "descricaoConta", "meta", "detalhes", "tipo"];
     
-    // Verifica se todos os campos obrigatórios estão preenchidos
     for (const field of fields) {
         if (document.getElementById(field).value === "") {
             alert(`${field} é obrigatório`);
             return false;
         }
     }
-    
-    // Verifica se um ícone foi selecionado
+
     const iconSelected = document.querySelector('.icon-picker input:checked');
     if (!iconSelected) {
         alert("Por favor, selecione um ícone.");
         return false;
     }
 
-    // Verifica se uma cor foi selecionada
     const colorSelected = document.querySelector('.color-picker input:checked');
     if (!colorSelected) {
         alert("Por favor, selecione uma cor.");
         return false;
     }
-
 
     return true;
 }
@@ -69,4 +47,28 @@ function addData() {
     }
 }
 
+function updatePreview() {
+    const iconPreview = document.getElementById("icon-preview");
+    const selectedIcon = document.querySelector('.icon-picker input:checked');
+    const selectedColor = document.querySelector('.color-picker input:checked');
 
+    if (selectedIcon) {
+        iconPreview.textContent = selectedIcon.value;
+    }
+
+    if (selectedColor) {
+        const colorValue = selectedColor.value;
+        document.querySelector('.preview-box').style.backgroundColor = colorValue;
+    }
+}
+
+const iconInputs = document.querySelectorAll('.icon-picker input');
+const colorInputs = document.querySelectorAll('.color-picker input');
+
+iconInputs.forEach(input => {
+    input.addEventListener('change', updatePreview);
+});
+
+colorInputs.forEach(input => {
+    input.addEventListener('change', updatePreview);
+});
